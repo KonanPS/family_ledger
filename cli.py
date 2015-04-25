c31f5f123589543efef76a48eea37dc00b429f88
pk <konan.ps@gmail.com>
2015-04-26 00:34:41 +0300

    Connection to db as a func. Connection and Cursor variables as a global variables. Establish connection onese for session.

--- cli.py
+++ cli.py
@@ -9,7 +9,19 @@ db_name = 'expenses_by_category.db'
 path_to_db = os.path.dirname(os.path.abspath(__file__)) + '/db/' + db_name
 print('path to db: ', path_to_db)
 
-def add_expense_to_db(string):
+def connect_to_db(path_to_db):
+	'''
+	Create connection to db. 
+	Create cursor for SQL queries with name 'cursor'.
+	Enable foreign keys.
+	'''
+	global connection, cursor 
+	connection = sqlite3.connect(path_to_db)
+	cursor = connection.cursor()
+	
+	cursor.execute('''PRAGMA foreign_keys = ON''') # enable foreign keys
+	
+def add_expense_to_db(string, cursor):
 	''' Get data for expense table 
 		Date, Total summ, Total discount 
 		as a string with space as separator
@@ -23,107 +35,71 @@ def add_expense_to_db(string):
 	data = string.split()
 	data[0] = time.mktime(datetime.datetime.strptime(data[0],'%d-%m-%Y').timetuple())
 	try:
-		connection = sqlite3.connect(path_to_db)
-		cursor = connection.cursor()
-		cursor.execute('''PRAGMA foreign_keys = ON''') # enable foreign keys
 		cursor.execute("""INSERT INTO expense (expense_date, total_sum, total_discount, category_id) VALUES (?,?,?,?)""", data)
 		connection.commit()
-		connection.close()
 	except sqlite3.IntegrityError:
 		print('No such category. Please add it')
 
 
-def list_expenses():
+def list_expenses(cursor):
 	''' Print records in expense table'''
 
-	connection = sqlite3.connect(path_to_db)
-	cursor = connection.cursor()
-	cursor.execute('''PRAGMA foreign_keys = ON''') # enable foreign keys
 	cursor.execute("""SELECT * FROM expense""")
 	res = cursor.fetchall()
 	for r in res:
 		print(r)
-	connection.close()
 
-def delete_expense(ex_id):
+def delete_expense(ex_id,cursor):
 	''' Delete expense from db via expense_id'''
 
-	connection = sqlite3.connect(path_to_db)
-	cursor = connection.cursor()
-	cursor.execute('''PRAGMA foreign_keys = ON''') # enable foreign keys
 	cursor.execute('''DELETE FROM expense WHERE expense_id = ?''', ex_id)
 	connection.commit()
-	connection.close()
 
-def update_expense(ex_id,d_str):
+def update_expense(ex_id,d_str,cursor):
 	'''Updates expense via expense_id and new data'''
 
 	data = d_str.split()
 	data.append(ex_id)
 
-	connection = sqlite3.connect(path_to_db)
-	cur = connection.cursor()
-	cur.execute('''PRAGMA foreign_keys = ON''') # enable foreign keys
-	cur.execute('UPDATE expense SET expense_date = ?, total_sum = ?, total_discount = ?, category_id = ? WHERE expense_id = ?', data)
+	cursor.execute('UPDATE expense SET expense_date = ?, total_sum = ?, total_discount = ?, category_id = ? WHERE expense_id = ?', data)
 	connection.commit()
-	connection.close()
 
-def read_expese(ex_id_string):
+def read_expese(ex_id_string,cursor):
 	''' Select expense record via expense_id '''
 
-	connection = sqlite3.connect(path_to_db)
-	cur = connection.cursor()
-	cur.execute('''PRAGMA foreign_keys = ON''') # enable foreign keys
-	cur.execute("SELECT * FROM expense WHERE expense_id = ?",ex_id_string)
-	r = cur.fetchall()
+	cursor.execute("SELECT * FROM expense WHERE expense_id = ?",ex_id_string)
+	r = cursor.fetchall()
 	print(r)
-	connection.close()
 
-def add_category(data_str):
+def add_category(data_str,cursor):
 	''' Add category'''
 
 	data = data_str.split()
-	connection = sqlite3.connect(path_to_db)
-	cur = connection.cursor()
-	cur.execute('''PRAGMA foreign_keys = ON''') # enable foreign keys
-	cur.execute('''INSERT INTO categories (name, parent_category_id) VALUES (?, ?)''', data)
+	cursor.execute('''INSERT INTO categories (name, parent_category_id) VALUES (?, ?)''', data)
 	connection.commit()
-	connection.close()
 
-def list_categories():
+def list_categories(cursor):
 	''' Print records in categories table'''
 
-	connection = sqlite3.connect(path_to_db)
-	cursor = connection.cursor()
-	cursor.execute('''PRAGMA foreign_keys = ON''') # enable foreign keys
 	cursor.execute("""SELECT * FROM categories""")
 	res = cursor.fetchall()
 	for r in res:
 		print(r)
-	connection.close()
 
-def delete_category(cat_id):
+def delete_category(cat_id,cursor):
 	''' Delete record from categories table via category_id'''
 
-	connection = sqlite3.connect(path_to_db)
-	cursor = connection.cursor()
-	cursor.execute('''PRAGMA foreign_keys = ON''') # enable foreign keys
 	cursor.execute('''DELETE FROM categories WHERE category_id = ?''', cat_id)
 	connection.commit()
-	connection.close()
 
-def update_category(cat_id, d_str):
+def update_category(cat_id, d_str,cursor):
 	'''Updates category via category_id and new data'''
 
 	data = d_str.split()
 	data.append(cat_id)
 
-	connection = sqlite3.connect(path_to_db)
-	cur = connection.cursor()
-	cur.execute('''PRAGMA foreign_keys = ON''') # enable foreign keys
 	cur.execute('UPDATE categories SET name = ?, parent_category_id = ? WHERE category_id = ?', data)
 	connection.commit()
-	connection.close()
 
 def print_menu():
 	''' Printing menu '''
@@ -150,49 +126,51 @@ def print_menu():
 
 
 print_menu()
+connect_to_db(path_to_db)
 
 while True:
 
 	action = input('Choose action> ')
 
 	if action == 'q':
+		connection.close()
 		break
 
 	elif action == '11':
 		a = input('Enter: expense_date total_sum total_discount category_id> ')
-		add_expense_to_db(a)
+		add_expense_to_db(a,cursor)
 
 	elif action == '12':
-		list_expenses()
+		list_expenses(cursor)
 
 	elif action == '13':
 		a = input('Enter expense id> ')
-		delete_expense(a)
+		delete_expense(a,cursor)
 
 	elif action == '14':
 		ex_id_string = input('Enter expens id> ')
 		a = input('Enter: expense_date total_sum total_discount category_id> ')
-		update_expense(ex_id_string,a)
+		update_expense(ex_id_string,a,cursor)
 
 	elif action == '15':
 		a = input('Enter expense id> ')
-		read_expese(a)
+		read_expese(a,cursor)
 
 	elif action == '21':
 		a = input('Enter: category_name parent_category_id> ')
-		add_category(a)
+		add_category(a,cursor)
 
 	elif action == '22':
-		list_categories()
+		list_categories(cursor)
 
 	elif action == '23':
 		a = input('Enter category_id> ')
-		delete_category(a)
+		delete_category(a,cursor)
 
 	elif action == '24':
 		cat_id_string = input('Enter expens id> ')
 		a = input('Enter: category_name parent_category_id> ')
-		update_category(cat_id_string,a)
+		update_category(cat_id_string,a,cursor)
 
 	elif action == 'm':
 		print_menu()
